63bee9fedfb1bc429879e18a6ef4288452680797
Mark Kim <marksk82@gmail.com>
2022-12-13 22:33:09 +0900

    Update Programming Assignment: Array and object iteration.js

--- Programming Assignment: Array and object iteration.js
+++ Programming Assignment: Array and object iteration.js
@@ -1,11 +1,12 @@
 // Task 1
 var dairy = ['cheese', 'sour cream', 'milk', 'yogurt', 'ice cream', 'milkshake'];
 
-function logDiary(dairy) {
+function logDairy(dairy) {
     for (let value of dairy) {
         console.log(value);
     }
 };
+logDairy();
 
 // Task 2
 const animal = {canJump: true};
@@ -13,15 +14,17 @@ const bird = Object.create(animal);
 bird.canFly = true;
 bird.hasFeathers = true;
 
-function birdCan(bird) {
-    for (let value of Object.keys(bird)) {
+function birdCan() {
+    for (value of Object.keys(bird)) {
         console.log(value, bird[value]);
     }        
-}
+};
+birdCan();
 
 // Task 3
-function animalCan(bird) {
-    for (let value in bird) {
+function animalCan() {
+    for (value in bird) {
         console.log(value);
     }        
-}
+};
+animalCan();
